ed85fe59853980ee99ce344ddfc360083d5404b3
Toby Inkster <mail@tobyinkster.co.uk>
2013-10-11 12:19:04 +0100

    bump version; changelog

--- lib/Switcheroo.pm
+++ lib/Switcheroo.pm
@@ -5,7 +5,7 @@ use warnings;
 package Switcheroo;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.007';
+our $VERSION   = '0.008';
 our @EXPORT    = qw( switch );
 our @EXPORT_OK = qw( match );
 our @ISA       = qw( Exporter::Tiny );
